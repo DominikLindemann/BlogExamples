c3c0dec85f113a28fd8fdc90a3910dd6a5ec1a06
Dominik Lindemann <dominik.lindemann@hotmail.de>
2015-10-23 20:51:21 +0200

    first checkin

---
+++ DoLi.FloatingPointNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DoLi.FloatingPointNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResultFalse();
+            Console.WriteLine("----------------------");
+            ResultTrue();
+            Console.ReadLine();
+        }
+
+
+        private static void ResultFalse()
+        {
+            double d1 = 1.000001F;
+            double d2 = 0.000001F;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine((d1 - d2) == 1.0);
+        }
+
+        private static void ResultTrue()
+        {
+            decimal d1 = 1.000001M;
+            decimal d2 = 0.000001M;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine((d1 - d2) == 1.0M);
+        }
+    }
+}
---
+++ DoLi.FloatingPointNumbers/README.md
@@ -0,0 +1,6 @@
+## Info ##
+Kleine Solution mit dem Beispielcoe zum meinem Quiz zu Gleitkommazahlen in .Net
+Die Frage hist hier zu finden [http://www.d-lindemann.de/blog/?p=22]
+
+Die Auflösung und erklärung ist hier nachzulesen [http://www.d-lindemann.de/blog/?p=24]
+
